8c01a8673cd08783874452877f4be7a4e5d44aa1
Nicki Lang <pandaunderwear@gmail.com>
2018-01-14 14:23:57 -0800

    More work done, at question 7E.

--- SQL_homework.sql
+++ SQL_homework.sql
@@ -82,18 +82,12 @@ as that is exactly what the actor will be with the grievous error.
 BE CAREFUL NOT TO CHANGE THE FIRST NAME OF EVERY ACTOR TO MUCHO GROUCHO, HOWEVER! 
 (Hint: update the record using a unique identifier.) */
 
-****************************************
-
-SELECT first_name FROM actor
-IF first_name = 'HARPO' THEN 
-    UPDATE actor 
-    SET first_name = REPLACE(first_name, 'HARPO', 'GROUCHO')
-ELSE
-    UPDATE actor 
-    SET first_name = REPLACE(first_name, 'HARPO', 'MUCHO GROUCHO')
-END IF;    
-
-****************************************
+SELECT first_name,
+CASE 
+    WHEN first_name = 'HARPO' THEN REPLACE(first_name, 'HARPO', 'GROUCHO')
+    ELSE REPLACE(first_name, 'HARPO', 'MUCHO GROUCHO')
+END 
+FROM actor;   
 
 /* 5a. You cannot locate the schema of the address table. Which query would you use to re-create it? */
 
@@ -112,15 +106,10 @@ ON a.address_id = s.address_id;
 /* 6b. Use JOIN to display the total amount rung up by each staff member in August of 2005. 
 Use tables staff and payment. */
 
-**************************************************
-
 SELECT s.first_name, s.last_name, SUM(p.amount) AS gross
 	FROM payment p
 	JOIN staff s ON (s.staff_id = p.staff_id)
-		WHERE p.payment_date > 08012005 AND p.payment_date < 08312005
-    GROUP BY s.staff_id;     
-
-***************************************************    
+    GROUP BY s.staff_id, DATE_FORMAT(p.payment_date, '%2005%August');    
 
 /* 6c. List each film and the number of actors who are listed for that film. 
 Use tables film_actor and film. Use inner join. */
@@ -132,6 +121,110 @@ SELECT f.title, COUNT(actor_id) AS Actors
 
 /* 6d. How many copies of the film Hunchback Impossible exist in the inventory system? */
 
+SELECT COUNT(*)
+FROM inventory
+WHERE film_id IN
+(
+    SELECT film_id
+    FROM film
+    WHERE title = 'Hunchback Impossible'
+);
+
 /* 6e. Using the tables payment and customer and the JOIN command, 
 list the total paid by each customer. 
 List the customers alphabetically by last name: */
+
+SELECT c.first_name, c.last_name, SUM(p.amount) AS gross 
+    FROM payment p 
+    JOIN customer c ON (c.customer_id = p.customer_id)
+	GROUP BY c.customer_id
+    ORDER BY c.last_name ASC;
+
+/* 7a. The music of Queen and Kris Kristofferson have seen an unlikely resurgence. 
+As an unintended consequence, films starting with the letters K and Q have also soared in popularity. 
+Use subqueries to display the titles of movies starting with the letters K and Q whose language is English. */
+
+SELECT title 
+FROM film
+WHERE film_id IN (
+    SELECT film_id
+    FROM film
+    WHERE (title LIKE 'Q%' OR title LIKE 'K%')
+);
+
+/* 7b. Use subqueries to display all actors who appear in the film Alone Trip. */
+
+SELECT first_name, last_name
+FROM actor
+WHERE actor_id IN
+(
+  SELECT actor_id
+  FROM film_actor
+  WHERE film_id IN
+  (
+    SELECT film_id
+    FROM film
+    WHERE title = 'ALONE TRIP'
+  )
+);
+
+/* 7c. You want to run an email marketing campaign in Canada, 
+for which you will need the names and email addresses of all Canadian customers. 
+Use joins to retrieve this information. */
+
+DROP TABLE IF EXISTS cust_country;
+
+CREATE TABLE cust_country
+SELECT c.first_name, c.last_name, c.email
+FROM customer c 
+    JOIN address a ON (c.address_id = a.address_id)
+    JOIN city ON (a.city_id = city.city_id)
+    JOIN country ON (city.country_id = country.country_id)
+    WHERE country.country = 'Canada';
+
+/* 7d. Sales have been lagging among young families, and you wish to target all family movies for a promotion. 
+Identify all movies categorized as famiy films. */
+
+SELECT title
+FROM film
+WHERE film_id IN
+(
+  SELECT film_id
+  FROM film_category
+  WHERE category_id IN
+  (
+    SELECT category_id
+    FROM category
+    WHERE name = 'FAMILY'
+  )
+);
+
+/* 7e. Display the most frequently rented movies in descending order. */
+
+
+
+/* 7f. Write a query to display how much business, in dollars, each store brought in. */
+
+
+
+/* 7g. Write a query to display for each store its store ID, city, and country. */
+
+
+
+/* 7h. List the top five genres in gross revenue in descending order. 
+(Hint: you may need to use the following tables: category, film_category, inventory, payment, and rental.) */
+
+
+
+/* 8a. In your new role as an executive, you would like to have an easy way of viewing the 
+Top five genres by gross revenue. 
+Use the solution from the problem above to create a view. 
+If you haven't solved 7h, you can substitute another query to create a view. */
+
+
+
+/* 8b. How would you display the view that you created in 8a? */
+
+
+
+/* 8c. You find that you no longer need the view top_five_genres. Write a query to delete it. */
